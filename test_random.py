e6fcc982928ab6dae11e58e1140dfdf54b532467
gepee <gurpindersinghk786@gmail.com>
2024-03-28 04:53:24 +0530

    updated GUI

---
+++ test_random.py
@@ -0,0 +1,5 @@
+a=5
+if type(a) is not 'str':
+    print("not a str")
+
+print("ok")
--- testapi.py
+++ testapi.py
@@ -6,6 +6,11 @@ import paho.mqtt.client as mqtt
 import csv
 import json
 import threading
+import pulp
+from tkinter import ttk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 broker_address = "broker.hivemq.com"
 broker_port = 1883
@@ -15,6 +20,154 @@ temp_sensor_id=None
 global sensor_data_url
 sensor_data_url = "http://localhost:5000/api/v3_0/sensors/data"
 
+def read_csv(filename):
+    with open(filename,'r') as file:
+        reader = csv.reader(file)
+        data = []
+        for row in reader:
+            print(row)
+        for element in row:
+            data.append(float(element))
+    print(data)
+    return data
+
+def downsampled_list(sereies):
+    downsampled_length = 24
+
+    segment_length = len(sereies) // downsampled_length
+    # Downsample the original list by calculating the average of each segment
+    downsampled_list = []
+    for i in range(downsampled_length):
+        start_index = i * segment_length
+        end_index = start_index + segment_length
+        segment = sereies[start_index:end_index]
+        average = sum(segment) / len(segment)
+        downsampled_list.append(average)
+    return downsampled_list
+
+
+    if type(data) is not 'str':
+        data = str(data)
+    return data
+
+def get_data(thing):
+    # makeitstr(sensorno)
+    # makeitstr(units)
+    # makeitstr(date)
+    # makeitstr(utc)
+    sensor_no = simpledialog.askstring("sensor_no","Enter the sensor no. for "+thing)
+    sensor_units = simpledialog.askstring("sensor_units","Enter the sensor units for "+thing)
+    date = simpledialog.askstring("date","Enter the date")
+    utc = simpledialog.askstring("utc","enter the utc")
+   
+
+    sensor_data_url = "http://localhost:5000/api/v3_0/sensors/data"
+    auth_url = "http://localhost:5000/api/requestAuthToken"
+    email = "projects.gepee@gmail.com"
+    password = "1234"
+    data_auth ={"email":email,"password":password}
+    auth_token = requests.post(auth_url, json=data_auth).json().get("auth_token")
+    sensor_id_com = "ea1.2024-04.localhost:fm1."+sensor_no
+    
+    print("                   ggg                ")
+
+    sensor_data = {
+        "sensor": sensor_id_com,
+        "start": date+"T00:00:00+"+utc,
+        "duration": "PT24H",
+        "resolution": "PT5M",
+        "unit": sensor_units
+        }
+
+    headers = {
+            "Authorization": auth_token
+        }
+
+    response = requests.get(sensor_data_url,params=sensor_data, headers=headers)
+
+    if response.status_code == 200:
+        print("Sensor data inserted successfully.",response.json()['values'])
+        display_message("upadted successfully for "+thing)
+    else:
+        print("Failed to insert sensor data: "+thing, response.json())
+        display_message(response.json)
+    return response.json()['values']
+def schedule():
+    Zmax = simpledialog.askfloat("battery cap", "enter the battery cap in MW")
+    Qmax = simpledialog.askinteger("battery charging rate", "enter the battery charging discharging rate")
+    is_solar = simpledialog.askstring("is_solar", "Is solar data avaialable yes or no")
+    T =24
+    if is_solar=='yes':
+        S = downsampled_list(get_data("solar"))
+    elif is_solar=='no':
+        S=[0 for i in range(0,T)]
+    is_wind = simpledialog.askstring("is_solar", "Is wind data avaialable yes or no")
+    if is_wind=='yes':
+        W = downsampled_list(get_data("wind"))
+    elif is_wind=='no':
+        W=[0 for i in range(0,T)]
+
+    P = downsampled_list(get_data("price"))
+    
+    L = downsampled_list(get_data("load"))
+    
+    Zmax =Zmax
+    Zmin = Zmax*0.05
+    Qmax = Qmax
+    Qmin = -Qmax
+    times = range(T)
+    model = pulp.LpProblem("Battery Scheduling", pulp.LpMinimize)
+    Q = pulp.LpVariable.dicts("Q", times, lowBound=Qmin, upBound=Qmax)
+    Z = pulp.LpVariable.dicts("Z", times, lowBound=Zmin, upBound=Zmax)
+
+    cost = pulp.lpSum(P[t]*(L[t] +S[t] + Q[t]-W[t]) for t in times)
+    model += cost
+
+    for t in times:
+        model += Z[t] >= Zmin
+        model += Z[t] <= Zmax
+        model += L[t] + S[t] + Q[t] -W[t] >= 0
+
+
+    for t in range(T-1):  # We iterate till T-1 because there's no t+1 for the last time step
+        model += Z[t+1] == Z[t] + Q[t]
+
+    
+    model.solve()
+    display_message("Total cost ="+str(pulp.value(model.objective)))
+
+    sensor_update = simpledialog.askstring("update_sensor","Which sensor you want to be updated")
+    date = simpledialog.askstring("uploading date","date on which the data to be uploaded")
+    sensor_data_url = "http://localhost:5000/api/v3_0/sensors/data"
+    auth_url = "http://localhost:5000/api/requestAuthToken"
+    email = "projects.gepee@gmail.com"
+    password = "1234"
+    data_auth ={"email":email,"password":password}
+    auth_token = requests.post(auth_url, json=data_auth).json().get("auth_token")
+    sensor_id_com = "ea1.2024-04.localhost:fm1."+sensor_update
+    # print("                   ggg                ")
+    # print(data)
+    sensor_data = {
+        "type": "PostSensorDataRequest",
+        "sensor": sensor_id_com,
+        "values": [pulp.value(Z[t]) for t in times],
+        "start": date+"T00:00:00+05:30",
+        "duration": "PT24H",
+        "unit": "MW"
+    }
+
+    headers = {
+        "Authorization": auth_token
+    }
+
+    response = requests.post(sensor_data_url, json=sensor_data, headers=headers)
+
+    if response.status_code == 200:
+        print("Sensor data inserted successfully.")
+        display_message("Sensor data inserted successfully.")
+    else:
+        print("Failed to insert sensor data:", response.json())
+        display_message("Failed to insert sensor data")
 
 def recieve_data():
     global temp_sensor_id, mqtt_loop_active, client
@@ -38,7 +191,6 @@ def recieve_data():
     else:
         client.loop_stop()
         display_message("stopped receiving")
-
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -173,7 +325,7 @@ def add_sensor():
         display_message("Failed to add sensor")
 
 def select_start_time():
-    start_time = simpledialog.askstring("Start Time", "Enter Start Time (YYYY-MM-DD HH:MM:SS):")
+    start_time = simpledialog.askstring("Start Time", "Enter Start date yyyy-mm-dd:")
     return start_time
 def units_():
     units = simpledialog.askstring("units","enter the units")
@@ -284,6 +436,8 @@ def display_message(message):
 
 window = tk.Tk()
 window.title("FlexMeasures Data Interface")
+add_scheduler_button = tk.Button(window, text="Schedule", command=schedule, height=2)
+add_scheduler_button.pack(pady=5)
 add_sensor_button = tk.Button(window, text="Add Sensor", command=add_sensor, height=2)
 add_sensor_button.pack(pady=5)
 post_button = tk.Button(window, text="Add Data", command=post_sensor_data, height=2)
@@ -295,5 +449,6 @@ reiceve_button.pack(pady=5)
 
 display = tk.Text(window, height=10, width=50)
 display.pack(pady=5)
+
 
 window.mainloop()
